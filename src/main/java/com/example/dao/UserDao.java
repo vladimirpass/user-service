c0fe2c8901aa1824eef7c59b9172e4badc2f6e3d
Vladimir Pasechnik <w.tiger.w@yandex.ru>
2026-02-05 12:04:24 +0300

    update

--- src/main/java/com/example/dao/UserDao.java
+++ src/main/java/com/example/dao/UserDao.java
@@ -5,45 +5,55 @@ import com.example.utils.HibernateUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Slf4j
 public class UserDao {
-    SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-
 
     public User saveUser(User user){
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = null;
+        try{
+            transaction = session.beginTransaction();
+            session.persist(user);
+            transaction.commit();
 
-        session.persist(user);
+        }catch(Exception e){
+            if(transaction != null) transaction.rollback();
+            throw new RuntimeException(e);
 
-        session.getTransaction().commit();
-        session.close();
+        }finally {
+            session.close();
+        }
         return user;
     }
 
     public void deleteUser(Integer id){
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-
-        if(session.get(User.class,id) != null) {
-            User userForDelete = session.get(User.class,id);
-            session.remove(userForDelete);
-        } else{
-            System.out.println("Пользователь с указанным индексом отсутствует.");
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = null;
+        try{
+            transaction = session.beginTransaction();
+            if(session.get(User.class,id) != null) {
+                User userForDelete = session.get(User.class,id);
+                session.remove(userForDelete);
+                transaction.commit();
+            } else{
+                System.out.println("Пользователь с указанным индексом отсутствует.");
+            }
+        }catch (Exception e){
+            if(transaction != null) transaction.rollback();
+            throw new RuntimeException(e);
+        }finally {
+            session.close();
         }
-
-        session.getTransaction().commit();
-        session.close();
     }
 
     public User getById(Integer id){
-        Session session = sessionFactory.openSession();
+        Session session = HibernateUtil.getSessionFactory().openSession();
         session.beginTransaction();
-
         if(session.get(User.class,id) != null) {
             User user = session.get(User.class,id);
             System.out.println(user);
@@ -57,7 +67,7 @@ public class UserDao {
     }
 
     public List<User> findAll(){
-        Session session = sessionFactory.openSession();
+        Session session = HibernateUtil.getSessionFactory().openSession();
         List<User> allUsers = session
                 .createQuery("SELECT s FROM User s", User.class)
                 .list();
@@ -66,20 +76,26 @@ public class UserDao {
     }
 
     public User updateUser(Integer id, User user){
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-
-        User userUpdate = session.get(User.class,id);
-        if(session.get(User.class,id) != null) {
-            userUpdate.setName(user.getName());
-            userUpdate.setAge(user.getAge());
-            userUpdate.setEmail(user.getEmail());
-            userUpdate.setCreateAt(LocalDateTime.now());
-        } else{
-            System.out.println("Отсутствует пользователь с введенным индексом");
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = null;
+        try{
+            transaction = session.beginTransaction();
+            User userUpdate = session.get(User.class,id);
+            if(session.get(User.class,id) != null) {
+                userUpdate.setName(user.getName());
+                userUpdate.setAge(user.getAge());
+                userUpdate.setEmail(user.getEmail());
+                userUpdate.setCreateAt(LocalDateTime.now());
+            } else{
+                System.out.println("Отсутствует пользователь с введенным индексом");
+            }
+            transaction.commit();
+        }catch (Exception e){
+            if (transaction != null) transaction.rollback();
+            throw new RuntimeException(e);
+        }finally {
+            session.close();
         }
-        session.getTransaction().commit();
-        session.close();
         return user;
     }
 
--- src/main/java/com/example/utils/HibernateUtil.java
+++ src/main/java/com/example/utils/HibernateUtil.java
@@ -4,26 +4,38 @@ import com.example.entity.User;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 
 public class HibernateUtil {
 
-    private static SessionFactory sessionFactory;
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+    private HibernateUtil() {
+    }
 
+    private static SessionFactory buildSessionFactory() {
+        try {
+            Configuration configuration = new Configuration();
+            configuration.configure("hibernate.cfg.xml");
+            configuration.addAnnotatedClass(User.class);
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties())
+                    .build();
+            return configuration.buildSessionFactory(serviceRegistry);
 
+        } catch (Exception e) {
+            System.err.println("Ошибка создания SessionFactory: " + e);
+            throw new ExceptionInInitializerError(e);
+        }
+    }
 
     public static SessionFactory getSessionFactory() {
-        if (sessionFactory == null) {
-            try {
-                Configuration configuration = new Configuration().configure();
-                configuration.addAnnotatedClass(User.class);
-                StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
-                sessionFactory = configuration.buildSessionFactory(builder.build());
-
-            } catch (Exception e) {
-                System.out.println("Исключение!" + e);
-            }
-        }
         return sessionFactory;
+    }
+
+    public static void shutdown() {
+        if (sessionFactory != null) {
+            sessionFactory.close();
+        }
     }
 
 }
